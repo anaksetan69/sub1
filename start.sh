321421ae2c08b111ef6dc78aa600bfd43a02df20
arifin6969 <118927237+anaksetan69@users.noreply.github.com>
2023-02-06 03:51:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-cd /app
-echo "https://github.com/anaksetan69/sub1.git"
-pymongo app:app & python3 -m sub1
+
+#! /bin/bash
+git clone https://github.com/anaksetan69/sub1.git ok &&  cd ok && pip3 install -U -r requirements.txt
+$START_CMD
